3bae32ccdbf8b87ebc095acbab037b42550cdcab
runlevl4 <root@runlevl4.com>
2020-04-19 00:20:51 -0400

    Refactored to use view template

--- main.go
+++ main.go
@@ -2,37 +2,36 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
+	"html/template"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
+
+var homeTemplate *template.Template
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	if err := homeTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Contact Us</h1>")
-}
-
-func faq(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Frequently Asked Questions</h1>")
-}
-
-func error(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>The gophers couldn't find what you were looking for. :-/</h1>")
+	fmt.Fprint(w, "To get in touch, please send an email "+
+		"to <a href=\"mailto:support@lenslocked.com\">"+
+		"support@lenslocked.com</a>.")
 }
 
 func main() {
-	var h http.Handler = http.HandlerFunc(error)
+	var err error
+	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
 	r := mux.NewRouter()
-
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/faq", faq)
-	r.NotFoundHandler = h
 	http.ListenAndServe(":3000", r)
 }
