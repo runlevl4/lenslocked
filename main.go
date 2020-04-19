3cf7d862d963c27b7ae783c2434343901d0a7397
runlevl4 <root@runlevl4.com>
2020-04-19 00:00:36 -0400

    Added faq; implemented 404 handler

--- main.go
+++ main.go
@@ -16,15 +16,23 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Us</h1>")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Frequently Asked Questions</h1>")
+}
+
 func error(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>The gophers couldn't find what you were looking for. :-/</h1>")
 }
 
 func main() {
+	var h http.Handler = http.HandlerFunc(error)
 	r := mux.NewRouter()
+
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
-
+	r.HandleFunc("/faq", faq)
+	r.NotFoundHandler = h
 	http.ListenAndServe(":3000", r)
 }
