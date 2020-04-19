f473e37f18e1fad0b59081580fe3e33aa88c9709
runlevl4 <root@runlevl4.com>
2020-04-18 23:50:06 -0400

    Refactored to use gorilla/mux router

--- main.go
+++ main.go
@@ -2,23 +2,29 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	switch r.URL.Path {
-	case "/":
-		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
-	case "/contact":
-		fmt.Fprint(w, "<h1>Contact Us</h1>")
-	default:
-		fmt.Fprint(w, "<h1>The gophers couldn't find what you were looking for. :-/</h1>")
-	}
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
 
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Contact Us</h1>")
+}
+
+func error(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>The gophers couldn't find what you were looking for. :-/</h1>")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+
+	http.ListenAndServe(":3000", r)
 }
